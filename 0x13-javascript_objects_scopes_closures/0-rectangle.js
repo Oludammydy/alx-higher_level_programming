5e300ea1cd64af4cabfa2f60d93acea9057b67e0
Oludammydy <oludammydy@gmail.com>
2022-09-06 08:16:34 -0700

    Rectangle #0 fixed

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./0-rectangle');
+class Rectangle {
+}
 
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+module.exports = Rectangle;
