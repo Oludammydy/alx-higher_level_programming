755479e3eeba41f6d338a5182d72585ffb3c2256
oludammydy <oludammydy@gmail.com>
2022-09-06 02:41:55 +0100

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let r = 0; r < size; r++) {
-	let row = '';
-	for (let c = 0; c < size; c++) row += 'X';
-	console.log(row);
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
