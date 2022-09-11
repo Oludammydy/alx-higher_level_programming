b13b7677cdee85289d10dd160ff51885d94bd478
Oluwadamilola Olaniyi <96773767+Oludammydy@users.noreply.github.com>
2022-09-11 02:43:07 +0100

    Get all States fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,13 @@ Script that lists all states from the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 
-if __name__== '__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states;")
-	states = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
 
-	for state in states:
-	    print(state)
+    for state in states:
+        print(state)
