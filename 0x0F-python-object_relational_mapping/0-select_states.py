9952b25c78696567db360e70e6ee5babc3e6e3cc
Oluwadamilola Olaniyi <96773767+Oludammydy@users.noreply.github.com>
2022-09-11 02:41:39 +0100

    Get all states fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ import sys
 import MySQLdb
 
 if __name__== '__main__':
-	db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                             db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
 	cur = db.cursor()
 	cur.execute("SELECT * FROM states;")
